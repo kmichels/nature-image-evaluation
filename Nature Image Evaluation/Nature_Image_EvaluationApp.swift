e7dd032fb0c23968153ebcb0b7f933ee4dad75df
kmichels <konrad.michels@me.com>
2025-10-27 10:37:27 -0400

    Initial Commit

---
+++ Nature Image Evaluation/Nature_Image_EvaluationApp.swift
@@ -0,0 +1,21 @@
+//
+//  Nature_Image_EvaluationApp.swift
+//  Nature Image Evaluation
+//
+//  Created by Konrad Michels on 10/27/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct Nature_Image_EvaluationApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
